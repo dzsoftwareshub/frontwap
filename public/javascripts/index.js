32c8727e38fc4eeb5c012f5d1db7424a1bfa25a0
Vikesh Kumar <dzsoftwareshub@gmail.com>
2022-02-24 10:55:45 +0530

    Something changed and new file created

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,36 +1,17 @@
-// requesting signup modal
+// login request modal
 $(document).ready(function(){
-	$("#request-signup-modal").click(function(e){
-		e.preventDefault();
-		$("#login-modal").modal("hide");
-		$("#signup-modal").modal("show");
-	});
-});
-
-
-// requesting login modal
-$(document).ready(function(){
-	$("#request-login-modal").click(function(e){
+	$(".request-login-modal").click(function(e){
 		e.preventDefault();
 		$("#signup-modal").modal("hide");
 		$("#login-modal").modal("show");
 	});
 });
 
-// signup
+// request signup modal
 $(document).ready(function(){
-	$("#signup-form").submit(function(e){
+	$(".request-signup-modal").click(function(e){
 		e.preventDefault();
-
-		$.ajax({
-			type : "POST",
-			url : "api/signup",
-			data : new FormData(this),
-			processData : false,
-			contentType : false,
-			success : function(response){
-				console.log(response);
-			}
-		});
+		$("#signup-modal").modal("show");
+		$("#login-modal").modal("hide");
 	});
 });
